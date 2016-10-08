6e245fdc0734fd03968ea5398dfceebabc33e443
BuildTools <unconfigured@null.spigotmc.org>
2016-10-08 11:58:10 -0400

    Datatypes

---
+++ Conquest/Conquest/Ability.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conquest
+{
+    public class Ability
+    {
+        private Action _abilityAction;
+        protected internal Action AbilityAction
+        {
+            get
+            {
+                return _abilityAction;
+            }
+            set
+            {
+                _abilityAction = value;
+            }
+        }
+
+        public Ability()
+        {
+
+        }
+
+        public virtual Ability CreateAbility()
+        {
+            return new Ability();
+        }
+    }
+}
--- Conquest/Conquest/Board.cs
+++ Conquest/Conquest/Board.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace Conquest
 {
-    static class FightManager
+    class Board
     {
-
     }
 }

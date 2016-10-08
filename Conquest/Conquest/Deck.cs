d929b3a037b40677ddf82f9002d5279a819f1015
Akash Shaji <akashshaji321@gmail.com>
2016-10-08 12:30:42 -0400

    Addded everything but initialization to Deck.CS

--- Conquest/Conquest/Deck.cs
+++ Conquest/Conquest/Deck.cs
@@ -11,6 +11,12 @@ namespace Conquest
     {
         private List<Card> _deck;
 
+        /// <summary>
+        /// Creates a deck with 26 cards
+        /// 2x each basic card
+        /// 4x hiers
+        /// 1x queen and king
+        /// </summary>
         public Deck()
         {
             for(int i = 0; i < 10; i++)
@@ -18,7 +24,10 @@ namespace Conquest
                
             }   
         }
-
+        /// <summary>
+        /// Returns a random card from the deck and removes it
+        /// </summary>
+        /// <returns>Random card</returns>
         public Card Draw()
         {
             if (_deck.Count == 0)
@@ -28,6 +37,12 @@ namespace Conquest
             _deck.RemoveAt(cardNumber);
             return r;
         }
-
+        /// <summary>
+        /// Adds a new card to the deck. 
+        /// </summary>
+        public void Add(Card _card)
+        {
+            _deck.Add(_card);
+        }
     }
 }

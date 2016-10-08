7b6c647505d99c85ff27f9627fff783d4922338e
Akash Shaji <akashshaji321@gmail.com>
2016-10-08 12:43:37 -0400

    added board stuff

--- Conquest/Conquest/Board.cs
+++ Conquest/Conquest/Board.cs
@@ -8,5 +8,17 @@ namespace Conquest
 {
     class Board
     {
+        private Slot[,] _board;
+
+        public Board(int h, int w)
+        {
+            _board = new Slot[h, w];
+        }
+
+//        public void PlaceCard(Point)
+//        {
+
+//        }
+
     }
 }

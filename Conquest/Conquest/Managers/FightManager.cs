adf27bb0ae26e989527f1beac4b1bb3aa80e7d70
Akash Shaji <akashshaji321@gmail.com>
2016-10-08 13:39:30 -0400

    sadfkl;j

--- Conquest/Conquest/Managers/FightManager.cs
+++ Conquest/Conquest/Managers/FightManager.cs
@@ -8,6 +8,40 @@ namespace Conquest
 {
     static class FightManager
     {
+        /// <summary>
+        /// Creature A is attacking B. 
+        /// Invincibulity only triggers when defending (so only B)
+        /// Returns 0 if B dies, 1 if A dies, 2 if Both die and 3 if neither die. 
+        /// </summary>
+        public static int Fight(Creature a, Creature b)
+        {
+            GameEventArgument defend = new GameEventArgument(b, a);
+            GameEventArgument attack = new GameEventArgument(a, b);
 
+            a.CreatureAttacks(attack);
+            b.CreatureIsAttacked(defend);
+                
+            if (b.Invicable)
+            {
+                if (b.Power >= a.Power)
+                    return 1;
+                else
+                    return 3;
+            }
+            if (a.Invicable)
+            {
+                if (a.Power >= b.Power)
+                    return 0;
+                else
+                    return 3;
+            }
+            if (a.Power == b.Power)
+                return 2;
+            if (a.Power > b.Power)
+                return 0;
+            else (b.Power > a.Power)
+                return 1;
+            return null;
+        }
     }
 }
